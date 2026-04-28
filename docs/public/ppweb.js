27013fb1ecefb0549cd542d353cecbd9df9a591d
Sjj1024 <15670339118@163.com>
2026-04-28 17:19:29 +0800

    Refine content retrieval in ppweb.js to access specific note properties for Chinese and English versions, enhancing localization accuracy.

--- docs/public/ppweb.js
+++ docs/public/ppweb.js
@@ -58,7 +58,9 @@ const initNotes = async () => {
     const urlHref = window.location.href
     const isChinese = urlHref.includes('zh')
     const ppPathName = window.location.pathname
-    const contentValue = isChinese ? ppnotesJsonData.zh : ppnotesJsonData.en
+    const contentValue = isChinese
+        ? ppnotesJsonData.zh.note
+        : ppnotesJsonData.en.note
     const titleValue = isChinese ? 'PackPlus公告' : 'PakePlus Notice'
     const okTextValue = isChinese ? '确定' : 'OK'
     if (
